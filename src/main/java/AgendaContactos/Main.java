ab5024cb6f0654b48dfc020531b76559af6bf639
joaomelro2 <joaomelro2@gmail.com>
2025-04-30 15:35:14 +0100

    Doc Main

--- src/main/java/AgendaContactos/Main.java
+++ src/main/java/AgendaContactos/Main.java
@@ -2,7 +2,19 @@ package AgendaContactos;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * Classe principal que inicia a aplicação da agenda de contactos.
+ * 
+ * A classe {@code Main} serve como ponto de entrada da aplicação. Ela inicializa a interface gráfica (GUI) da aplicação
+ * utilizando a classe {@link ContatoGui}. A execução da GUI é feita na thread de eventos do Swing através do método
+ * {@link SwingUtilities#invokeLater}.
+ */
 public class Main {
+    /**
+     * Método principal que inicia a aplicação.
+     * 
+     * @param args Argumentos passados pela linha de comandos. Não são utilizados neste caso.
+     */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(ContatoGui::new);
     }
